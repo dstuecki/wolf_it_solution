76604afbb224f2ab4f0d2c7644eea1080ab0c3de
dstuecki <dstuecki@gmail.com>
2024-06-20 07:24:37 +0200

    Meta Daten ausgelagert

---
+++ incs/meta.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8" />
+<meta name="author" content="www.frebsite.nl" />
+<meta name="viewport" content="width=device-width minimum-scale=1.0 maximum-scale=1.0 user-scalable=no" />
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="css/demo.css" />
+<link rel="stylesheet" href="css/mmenu.css" />
+<link rel="stylesheet" href="css/styles.css" />
--- index.php
+++ index.php
@@ -2,16 +2,11 @@
 <!DOCTYPE html>
 <html> 
     <head>
-        <meta charset="utf-8" />
-        <meta name="author" content="www.frebsite.nl" />
-        <meta name="viewport" content="width=device-width minimum-scale=1.0 maximum-scale=1.0 user-scalable=no" />
-
+        <?php
+            include ('incs/meta.php');
+        ?>
         <title>Star Citzen Tools</title>
 
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="css/demo.css" />
-        <link rel="stylesheet" href="css/mmenu.css" />
-		<link rel="stylesheet" href="css/styles.css" />
         <style>
             .mm-wrapper--sidebar-expanded.mm-wrapper--opened #header a {
                 display: none;
